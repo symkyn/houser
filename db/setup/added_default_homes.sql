1a504dca48b129b42128d3d4eaeb11563598aa0b
Simon <everett.blake@gmail.com>
2018-07-05 20:54:22 -0600

    setup db

---
+++ db/setup/added_default_homes.sql
@@ -0,0 +1,21 @@
+insert into "Houses" (
+	name,
+	address,
+	city,
+	state,
+	zip
+)
+values (
+	'Dallas Home',
+	'1210 Cannes Pl',
+	'Carrollton',
+	'TX',
+	75006
+),
+(
+	'Utah Home',
+	'870 S 500 E',
+	'Orem',
+	'UT',
+	84097
+)
---
+++ db/setup/create_houses_table.sql
@@ -0,0 +1,8 @@
+create table "Houses" (
+	id serial primary key,
+	name varchar(30),
+	address varchar(100),
+	city varchar(100),
+	state varchar(2),
+	zip integer
+);
