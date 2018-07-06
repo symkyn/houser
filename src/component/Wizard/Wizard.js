4149879812598097ddd3c3881926a42db248a9a4
Simon <everett.blake@gmail.com>
2018-07-05 20:40:25 -0600

    part 1 step 2

--- src/component/Wizard/Wizard.js
+++ src/component/Wizard/Wizard.js
@@ -3,19 +3,73 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Wizard extends Component {
+    inputs = [
+        {
+            label: 'Name',
+            property: 'name'
+        },
+        {
+            label: 'Address',
+            property: 'address'
+        },
+        {
+            label: 'City',
+            property: 'city'
+        },
+        {
+            label: 'State',
+            property: 'state'
+        },
+        {
+            label: 'Zip Code',
+            property: 'zipcode'
+        }
+    ]
 
+    constructor(){
+        super();
+        
+    this.state={
+        name: '',
+        address: '',
+        city: '',
+        state: '',
+        zipcode: 0
+    }
+}
 
 
 render() {
+    const inputs = this.inputs
+        .map((input, i) => (
+            <div key={`new-house-form-${i}`}>
+                <label>
+                    {input.label}:
+                    <input 
+                        value={this.state[input.property]}
+                        onChange={e => this.handleChange(e, input.property)}
+                        name={input.property}
+                    />
+                </label>
+            </div>
+        ))
     return (
         <div>
             Wizard
+            {inputs}
             <Link to='/'>
                 <button>Cancel</button>
             </Link>
         </div>
     )
 }
+
+handleChange(e, name) {
+    this.setState({ [name]: e.target.value});
 }
+
+}
+
+
 
 export default Wizard;
